e74d0f74ab0f87ef665213bb7c8d3c60ff22d387
Manière Louis <lmanie01@dsi.ens-lyon.fr>
2023-08-30 17:30:28 +0200

    update only polyline color and add landuse metrics to explore

--- R/mod_explore.R
+++ R/mod_explore.R
@@ -10,7 +10,7 @@
 #' @importFrom leaflet leafletOutput renderLeaflet addProviderTiles colorQuantile
 #' @importFrom shiny NS tagList
 #' @importFrom plotly plotlyOutput
-#' @importFrom dplyr left_join
+#' @importFrom dplyr left_join right_join
 #' @import sf
 #' @import mapdotoro
 mod_explore_ui <- function(id){
@@ -24,8 +24,8 @@ mod_explore_ui <- function(id){
         column(
           width = 3,
           titlePanel("Metriques"),
-          selectInput(ns("choice"), "Sélectionez une métrique :",
-                      choices = c("Largeurs", "Pentes"),
+          selectInput(ns("metric"), "Sélectionez une métrique :",
+                      choices = c("Largeurs", "Pentes", "Occupation du sol"),
                       selected  = "Largeurs"), # selectInput for dynamic radio buttons
           uiOutput(ns("radioButtonsUI")
                    ) # uiOutput radios buttons metrics
@@ -62,10 +62,16 @@ mod_explore_server <- function(input, output, session){
 
     ns <- session$ns
 
-    output$radioButtonsUI <- renderUI({
-      selected_choice <- input$choice
+    output$ui_exploremap <- renderLeaflet({
+      leaflet() %>%
+        setView(lng = 2.468697, lat = 46.603354, zoom = 6) %>%
+        addTiles()
+    })
 
-      if (selected_choice == "Largeurs") {
+    output$radioButtonsUI <- renderUI({
+      selected_metric <- input$metric
+
+      if (selected_metric == "Largeurs") {
         radioButtons(ns("dynamicRadio"), "Largeurs :",
                      choiceNames = c(
                        "Chenal actif",
@@ -78,7 +84,7 @@ mod_explore_server <- function(input, output, session){
                                          "connected_corridor_width",
                                          "valley_bottom_width"),
                      selected = "active_channel_width")
-      } else if (selected_choice == "Pentes") {
+      } else if (selected_metric == "Pentes") {
         radioButtons(ns("dynamicRadio"), "Pentes :",
                      choiceNames = c(
                        "Pente du talweg",
@@ -88,38 +94,95 @@ mod_explore_server <- function(input, output, session){
                                          "floodplain_slope"),
                      selected = "talweg_slope"
         )
-      } else {
-        # Default case
-        NULL
+      } else if (selected_metric == "Occupation du sol") {
+        radioButtons(ns("dynamicRadio"), "Occupation du sol :",
+                     choiceNames = c(
+                       "Surface en eau",
+                       "Bancs sédimentaires",
+                       "Espace naturel ouvert",
+                       "Forêt",
+                       "Prairie permanente",
+                       "Culture",
+                       "Périurbain",
+                       "Urbain dense",
+                       "Infrastructure de stransport"
+                     ),
+                     choiceValues = list("Water Channel",
+                                         "Gravel Bars",
+                                         "Natural Open",
+                                         "Forest",
+                                         "Grassland",
+                                         "Crops",
+                                         "Diffuse Urban",
+                                         "Dense Urban",
+                                         "Infrastructures"),
+                     selected = "Water Channel"
+        )
       }
     })
 
-    datamap <- network_data %>%
-      left_join(metrics_data, by = join_by("AXIS"=="AXIS", "M"=="measure"),
-                suffix = c("", ".metrics"), relationship="one-to-one")
-
-    var <- reactive({
-      switch(input$dynamicRadio,
-             # Width
-             "active_channel_width" = datamap$active_channel_width,
-             "natural_corridor_width" = datamap$natural_corridor_width,
-             "connected_corridor_width" = datamap$connected_corridor_width,
-             "valley_bottom_width" = datamap$valley_bottom_width,
-             # Slope
-             "talweg_slope" = datamap$talweg_slope,
-             "floodplain_slope" = datamap$floodplain_slope
-      )
+    datamap <- reactive({
+      if (input$metric == "Largeurs" | input$metric == "Pentes") {
+        network_data %>%
+          left_join(metrics_data, by = join_by("AXIS"=="AXIS", "M"=="measure"),
+                    suffix = c("", ".metrics"), relationship="one-to-one")
+      } else if (input$metric == "Occupation du sol") {
+        landcover_data %>%
+          filter(landcover == input$dynamicRadio) %>%
+          right_join(network_data, by = join_by("AXIS"=="AXIS", "measure"=="M"),
+                    suffix = c("", ".landcover"), relationship="one-to-one") %>%
+          st_as_sf()
+      }
     })
 
-    output$ui_exploremap <- renderLeaflet({
-      qpal <- colorQuantile("Reds", domain = var(), n = 5, na.color = "#808080")
-            leaflet() %>%
-              setView(lng = 2.468697, lat = 46.603354, zoom = 6) %>%
-              addTiles() %>%
-              addPolylines(data = datamap, color = ~qpal(var()))
-    }) # ui_exploremap
+    varsel <- reactive({
+      if (input$metric == "Largeurs" | input$metric == "Pentes") {
+        print(input$dynamicRadio)
+        datamap()[[input$dynamicRadio]]
+      } else if (input$metric == "Occupation du sol") {
+        print(input$dynamicRadio)
+        datamap()[["landcover_area"]]
+      }
+    })
+
+    observeEvent(input$dynamicRadio, {
+      if (input$metric == "Largeurs" | input$metric == "Pentes") {
+
+        breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
+
+        # Define color palette for Reds
+        color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
+
+        leafletProxy("ui_exploremap") %>%
+          clearShapes() %>%
+          addPolylines(data = datamap(), color = ~ {
+            ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
+          }
+              )
+
+      } else if (input$metric == "Occupation du sol") {
+
+        # qpal <- colorQuantile("Reds", domain = varsel(), n = 5, na.color = "#808080")
+        #
+        # breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
+        # print(breaks)
+
+        breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
+
+        # Define color palette for Reds
+        color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
+
+        leafletProxy("ui_exploremap") %>%
+          clearShapes() %>%
+          addPolylines(data = datamap(), color = ~ {
+            ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
+          }
+          )
+      }
+    })
 
   }
+
 
 ## To be copied in the UI
 # mod_explore_ui("explore_1")
