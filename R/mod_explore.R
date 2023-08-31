e5da6716f6f7caaf380c34fb146ed0df1f8dcf0f
Manière Louis <lmanie01@dsi.ens-lyon.fr>
2023-08-31 09:28:22 +0200

    cleanning and comment mod_explore

--- R/mod_explore.R
+++ R/mod_explore.R
@@ -28,7 +28,7 @@ mod_explore_ui <- function(id){
                       choices = c("Largeurs", "Pentes", "Occupation du sol"),
                       selected  = "Largeurs"), # selectInput for dynamic radio buttons
           uiOutput(ns("radioButtonsUI")
-                   ) # uiOutput radios buttons metrics
+          ) # uiOutput radios buttons metrics
         ), # column
         column(
           width = 6,
@@ -50,7 +50,7 @@ mod_explore_ui <- function(id){
         )# tabsetPanel
       )# fluidRow
     ) # fluidPage
-) # tagList
+  ) # tagList
 }
 
 #' explore Server Functions
@@ -60,128 +60,128 @@ mod_explore_ui <- function(id){
 # mod_explore_server <- function(id){
 mod_explore_server <- function(input, output, session){
 
-    ns <- session$ns
+  ns <- session$ns
 
-    output$ui_exploremap <- renderLeaflet({
-      leaflet() %>%
-        setView(lng = 2.468697, lat = 46.603354, zoom = 6) %>%
-        addTiles()
-    })
+  # mapping initialization
+  output$ui_exploremap <- renderLeaflet({
+    leaflet() %>%
+      setView(lng = 2.468697, lat = 46.603354, zoom = 6) %>%
+      addTiles()
+  })
 
-    output$radioButtonsUI <- renderUI({
-      selected_metric <- input$metric
+  # metrics radio buttons UI
+  output$radioButtonsUI <- renderUI({
+    selected_metric <- input$metric
 
-      if (selected_metric == "Largeurs") {
-        radioButtons(ns("dynamicRadio"), "Largeurs :",
-                     choiceNames = c(
-                       "Chenal actif",
-                       "Corridor naturel",
-                       "Corridor connecté",
-                       "Fond de vallée"
-                     ),
-                     choiceValues = list("active_channel_width",
-                                         "natural_corridor_width",
-                                         "connected_corridor_width",
-                                         "valley_bottom_width"),
-                     selected = "active_channel_width")
-      } else if (selected_metric == "Pentes") {
-        radioButtons(ns("dynamicRadio"), "Pentes :",
-                     choiceNames = c(
-                       "Pente du talweg",
-                       "Pente du fond de valée"
-                     ),
-                     choiceValues = list("talweg_slope",
-                                         "floodplain_slope"),
-                     selected = "talweg_slope"
+    if (selected_metric == "Largeurs") {
+      radioButtons(ns("dynamicRadio"), "Largeurs :",
+                   choiceNames = c(
+                     "Chenal actif",
+                     "Corridor naturel",
+                     "Corridor connecté",
+                     "Fond de vallée"
+                   ),
+                   choiceValues = list("active_channel_width",
+                                       "natural_corridor_width",
+                                       "connected_corridor_width",
+                                       "valley_bottom_width"),
+                   selected = "active_channel_width")
+    } else if (selected_metric == "Pentes") {
+      radioButtons(ns("dynamicRadio"), "Pentes :",
+                   choiceNames = c(
+                     "Pente du talweg",
+                     "Pente du fond de valée"
+                   ),
+                   choiceValues = list("talweg_slope",
+                                       "floodplain_slope"),
+                   selected = "talweg_slope"
+      )
+    } else if (selected_metric == "Occupation du sol") {
+      radioButtons(ns("dynamicRadio"), "Occupation du sol :",
+                   choiceNames = c(
+                     "Surface en eau",
+                     "Bancs sédimentaires",
+                     "Espace naturel ouvert",
+                     "Forêt",
+                     "Prairie permanente",
+                     "Culture",
+                     "Périurbain",
+                     "Urbain dense",
+                     "Infrastructure de stransport"
+                   ),
+                   choiceValues = list("Water Channel",
+                                       "Gravel Bars",
+                                       "Natural Open",
+                                       "Forest",
+                                       "Grassland",
+                                       "Crops",
+                                       "Diffuse Urban",
+                                       "Dense Urban",
+                                       "Infrastructures"),
+                   selected = "Water Channel"
+      )
+    }
+  })
+
+  # dataset
+  datamap <- reactive({
+    if (input$metric == "Largeurs" | input$metric == "Pentes") {
+      network_data %>%
+        left_join(metrics_data, by = join_by("AXIS"=="AXIS", "M"=="measure"),
+                  suffix = c("", ".metrics"), relationship="one-to-one")
+    } else if (input$metric == "Occupation du sol") {
+      landcover_data %>%
+        filter(landcover == input$dynamicRadio) %>%
+        right_join(network_data, by = join_by("AXIS"=="AXIS", "measure"=="M"),
+                   suffix = c("", ".landcover"), relationship="one-to-one") %>%
+        st_as_sf()
+    }
+  })
+
+  # metrics data to display
+  varsel <- reactive({
+    if (input$metric == "Largeurs" | input$metric == "Pentes") {
+      print(input$dynamicRadio)
+      datamap()[[input$dynamicRadio]]
+    } else if (input$metric == "Occupation du sol") {
+      print(input$dynamicRadio)
+      datamap()[["landcover_area"]]
+    }
+  })
+
+  # mapping interactive change
+  observeEvent(input$dynamicRadio, {
+    if (input$metric == "Largeurs" | input$metric == "Pentes") {
+
+      breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
+
+      # Define color palette for Reds
+      color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
+
+      leafletProxy("ui_exploremap") %>%
+        clearShapes() %>%
+        addPolylines(data = datamap(), color = ~ {
+          ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
+        }
         )
-      } else if (selected_metric == "Occupation du sol") {
-        radioButtons(ns("dynamicRadio"), "Occupation du sol :",
-                     choiceNames = c(
-                       "Surface en eau",
-                       "Bancs sédimentaires",
-                       "Espace naturel ouvert",
-                       "Forêt",
-                       "Prairie permanente",
-                       "Culture",
-                       "Périurbain",
-                       "Urbain dense",
-                       "Infrastructure de stransport"
-                     ),
-                     choiceValues = list("Water Channel",
-                                         "Gravel Bars",
-                                         "Natural Open",
-                                         "Forest",
-                                         "Grassland",
-                                         "Crops",
-                                         "Diffuse Urban",
-                                         "Dense Urban",
-                                         "Infrastructures"),
-                     selected = "Water Channel"
+
+    } else if (input$metric == "Occupation du sol") {
+
+      breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
+
+      # Define color palette for Reds
+      color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
+
+      leafletProxy("ui_exploremap") %>%
+        clearShapes() %>%
+        addPolylines(data = datamap(), color = ~ {
+          ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
+        }
         )
-      }
-    })
+    }
+  }) # ObserveEvent
 
-    datamap <- reactive({
-      if (input$metric == "Largeurs" | input$metric == "Pentes") {
-        network_data %>%
-          left_join(metrics_data, by = join_by("AXIS"=="AXIS", "M"=="measure"),
-                    suffix = c("", ".metrics"), relationship="one-to-one")
-      } else if (input$metric == "Occupation du sol") {
-        landcover_data %>%
-          filter(landcover == input$dynamicRadio) %>%
-          right_join(network_data, by = join_by("AXIS"=="AXIS", "measure"=="M"),
-                    suffix = c("", ".landcover"), relationship="one-to-one") %>%
-          st_as_sf()
-      }
-    })
-
-    varsel <- reactive({
-      if (input$metric == "Largeurs" | input$metric == "Pentes") {
-        print(input$dynamicRadio)
-        datamap()[[input$dynamicRadio]]
-      } else if (input$metric == "Occupation du sol") {
-        print(input$dynamicRadio)
-        datamap()[["landcover_area"]]
-      }
-    })
-
-    observeEvent(input$dynamicRadio, {
-      if (input$metric == "Largeurs" | input$metric == "Pentes") {
-
-        breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
-
-        # Define color palette for Reds
-        color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
-
-        leafletProxy("ui_exploremap") %>%
-          clearShapes() %>%
-          addPolylines(data = datamap(), color = ~ {
-            ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
-          }
-              )
-
-      } else if (input$metric == "Occupation du sol") {
-
-        # qpal <- colorQuantile("Reds", domain = varsel(), n = 5, na.color = "#808080")
-        #
-        # breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
-        # print(breaks)
-
-        breaks <-  unique(quantile(varsel(), probs = seq(0, 1, 0.25), na.rm = TRUE))
-
-        # Define color palette for Reds
-        color_palette <- colorRampPalette(c("green", "red"))(length(breaks))
-
-        leafletProxy("ui_exploremap") %>%
-          clearShapes() %>%
-          addPolylines(data = datamap(), color = ~ {
-            ifelse(is.na(varsel()), "grey", color_palette[findInterval(varsel(), breaks, all.inside = TRUE)])
-          }
-          )
-      }
-    })
-
-  }
+}
 
 
 ## To be copied in the UI
