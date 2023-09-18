7539c81de99b9b9769927bc71e9686e3cf91d1ad
Manière Louis <lmanie01@dsi.ens-lyon.fr>
2023-09-18 14:03:01 +0200

    fix decoding app and description in server dev

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+Sys.setlocale("LC_ALL","fr_FR.UTF-8")
+
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 mapdoapp::run_app() # add parameters here (if any)
