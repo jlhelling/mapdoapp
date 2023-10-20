0b45cfa8e07dcbf02218cf440e2a46af65aa6856
Manière Louis <louismaniere@orange.fr>
2023-10-20 15:04:08 +0200

    add bassin_hydrographique data

---
+++ R/doc_bassin_hydrographique.R
@@ -0,0 +1,15 @@
+#' bassin_hydrographique
+#'
+#' Description.
+#'
+#' @format A data frame with 1 rows and 6 variables:
+#' \describe{
+#'   \item{ gid }{  numeric }
+#'   \item{ cdbh }{  character }
+#'   \item{ lbbh }{  character }
+#'   \item{ numcircadm }{  character }
+#'   \item{ projcoordo }{  character }
+#'   \item{ geom }{  sfc_MULTIPOLYGON,sfc }
+#' }
+#' @source Source
+"bassin_hydrographique"
---
+++ data-raw/bassin_hydrographique.R
@@ -0,0 +1,9 @@
+## code to prepare `bassin_hydrographique` dataset goes here
+
+query <- "SELECT * FROM bassin_hydrographique"
+bassin_hydrographique <- sf::st_read(dsn = db_con(), query = query) %>%
+  dplyr::filter(cdbh == "06")
+
+usethis::use_data(bassin_hydrographique, overwrite = TRUE)
+checkhelper::use_data_doc(name = "bassin_hydrographique")
+attachment::att_amend_desc()
