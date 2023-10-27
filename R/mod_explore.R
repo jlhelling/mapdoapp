750d86780ca5d8b437ea4539d69329f56b2ea882
Manière Louis <louismaniere@orange.fr>
2023-10-27 14:26:43 +0200

    mettric button in observe event

--- R/mod_explore.R
+++ R/mod_explore.R
@@ -121,7 +121,9 @@ mod_explore_server <- function(id){
       selected_profile_metric_name = NULL,
       select_profile_metric_category = NULL,
       strahler = NULL,
-      ui_strahler_slider = NULL
+      ui_strahler_slider = NULL,
+      ui_metric_selectInput = NULL,
+      ui_metric_radioButtons = NULL
     )
 
     ### BASSIN INIT MAP ####
@@ -167,12 +169,9 @@ mod_explore_server <- function(id){
 
     # UI create choose metric
     output$metricUI <- renderUI({
-
       # wait region hydrographic to display metric selection
-      if (!is.null(r_val$region_click)){
-      selectInput(ns("metric_type"), "Sélectionez une métrique :",
-                  choices = names(params_metrics_choice()),
-                  selected  = names(params_metrics_choice())[1])
+      if (!is.null(r_val$ui_metric_selectInput)){
+        r_val$ui_metric_selectInput
       } else {
         HTML('<label class="control-label" id="wait-metric-label">
              Cliquez sur une région hydrographique pour afficher la sélection des métriques</label>')
@@ -181,15 +180,7 @@ mod_explore_server <- function(id){
 
     # UI metrics radio buttons
     output$radioButtonsUI <- renderUI({
-
-      req(input$metric_type)
-
-      metric_type <- input$metric_type
-
-      radioButtons(ns("metric"), sprintf("%s :", metric_type),
-                   choiceNames = as.list(unname(params_metrics_choice()[[metric_type]])),
-                   choiceValues = names(params_metrics_choice()[[metric_type]]),
-                   selected = character(0))
+      r_val$ui_metric_radioButtons
     })
 
     # UI switch unit area
@@ -267,6 +258,11 @@ mod_explore_server <- function(id){
           map_region_clicked(region_click = input$exploremap_shape_click,
                              selected_region_feature = r_val$selected_region_feature)
 
+        # build metric selectInput
+        r_val$ui_metric_selectInput = selectInput(ns("metric_type"), "Sélectionez une métrique :",
+                                             choices = names(params_metrics_choice()),
+                                             selected  = names(params_metrics_choice())[1])
+
       }
       ### axis clicked ####
       if (input$exploremap_shape_click$group == params_map_group()$axis) {
@@ -287,6 +283,16 @@ mod_explore_server <- function(id){
           map_axis_start_end(axis_start_end = r_val$axis_start_end, region_axis = r_val$network_region_axis)
       }
     })
+
+    ### EVENT METRIC TYPE SELECT ####
+    observeEvent(input$metric_type, {
+      # build metric radioButtons
+      r_val$ui_metric_radioButtons = radioButtons(ns("metric"), sprintf("%s :", input$metric_type),
+                                                  choiceNames = as.list(unname(params_metrics_choice()[[input$metric_type]])),
+                                                  choiceValues = names(params_metrics_choice()[[input$metric_type]]),
+                                                  selected = character(0))
+    })
+
 
     ### EVENT METRIC SELECT ####
 
