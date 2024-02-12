841bf73e49125fa48752200e8c5662e0a3ba53eb
Manière Louis <louismaniere@orange.fr>
2024-02-12 14:21:32 +0100

    test utile_normalize_string

--- tests/testthat/test-fct_utils.R
+++ tests/testthat/test-fct_utils.R
@@ -3,3 +3,9 @@ test_that("data_get_dgo_in_region works", {
   expect_true(inherits(url, "character"),
               "character url loaded")
 })
+
+test_that("data_get_dgo_in_region works", {
+  original_string <- "Thïs is à sâmplè strîng with spèciál chàracters!"
+  normalized_string <- utile_normalize_string(original_string)
+  expect_true(normalized_string == "thisisasamplestringwithspecialcharacters")
+})
