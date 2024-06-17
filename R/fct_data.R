9465de8686b19f709cf3c6f6d004381b79e909d5
jlhelling <128162318+jlhelling@users.noreply.github.com>
2024-06-17 18:55:54 +0200

    rhandsontable showing classes in classification pane. error: when selecting other metric the app crashes

--- R/fct_data.R
+++ R/fct_data.R
@@ -443,3 +443,53 @@ data_get_elevation_profiles <- function(selected_dgo_fid, con){
     mutate(profile = round(profile, digits = 2))
   return(data)
 }
+
+
+#' Create initial dataframe for 1-variable classification, to be displayed on the UI table
+#'
+#' @param axis_data sf-object of an axis, containing all dgos inside the axis
+#' @param variable_name name of variable for which the classification should be undertaken
+#' @param no_classes number of classes to be generated
+#' @param quantile size of quantile which provides value-range of classification
+#'
+#' @return dataframe with 4 columns: class (name of each class, here automatically set from A-Z),
+#'         variable (variable chosen for classification), greaterthan (values defining the threshold of each class),
+#'         and color (defining the coloring for the map)
+#'
+#' @importFrom RColorBrewer brewer.pal
+#'
+#' @examples
+#'df <- create_df_input(
+#'       axis_data = network_dgo,
+#'       variable_name = input$variable,
+#'       no_classes = input$no_classes,
+#'       quantile = input$quantile
+#'       )
+#'
+create_df_input <- function(axis_data, variable_name, no_classes, quantile = 95){
+
+  # set upper and lower boundaries of quantile interval
+  q_low <- (1 - quantile/100)/2
+  q_high <- 1 - q_low
+
+  # calculate quantile values (max, min) and steps
+  q_values <- quantile(axis_data[[variable_name]], probs = c(q_low, q_high), na.rm = TRUE)
+  q_steps <- (q_values[[2]] - q_values[[1]])/no_classes
+
+  # empty dataframe to store class thresholds
+  classes <- rep(0, no_classes)
+
+  # set threshold values of all classes
+  for (i in 1:no_classes) {
+    classes[i] <- q_steps*(no_classes - i)
+  }
+
+  # create dataframe
+  df <- data.frame(class = LETTERS[1:no_classes],
+                   variable = variable_name,
+                   greaterthan = classes,
+                   color = {if (no_classes == 2) {c("#B2182B", "#2166AC")} else {brewer.pal(no_classes, "RdBu")}},
+                   stringsAsFactors = FALSE)
+
+  return(df)
+}
--- R/mod_metric_analysis.R
+++ R/mod_metric_analysis.R
@@ -43,7 +43,8 @@ mod_metric_analysis_ui <- function(id){
 #'
 #' @import shiny
 #' @importFrom htmltools HTML div img
-#' @importFrom dplyr filter mutate if_else pull
+#' @importFrom dplyr filter mutate if_else pull bind_cols
+#' @importFrom rhandsontable rHandsontableOutput rhandsontable hot_context_menu renderRHandsontable hot_to_r
 #' @importFrom shinyjs onclick runjs
 #' @noRd
 mod_metric_analysis_server <- function(id, r_val){
@@ -60,8 +61,12 @@ mod_metric_analysis_server <- function(id, r_val){
 
       # plots
       violinplots_metric = NULL,
-      barplots_classes_metric = NULL
+      barplots_classes_metric = NULL,
 
+      # classification
+      # grouping
+      man_grouping_editable_table = NULL,
+      grouping_table_data = NULL
     )
 
     ### OUTPUTS ####
@@ -85,6 +90,15 @@ mod_metric_analysis_server <- function(id, r_val){
       r_val_local$violinplots_metric
     })
 
+    # editable table for classification
+    output$man_grouping_editable_tableUI <- renderUI({
+      r_val_local$man_grouping_editable_table
+    })
+
+
+    ### EVENTS ####
+
+    #### axis clicked ####
     observe({
 
       if (!is.null(r_val$axis_click)) {
@@ -99,7 +113,10 @@ mod_metric_analysis_server <- function(id, r_val){
       }
     })
 
+    #### metric changed ####
     observeEvent(input$metric, {
+
+      r_val$selected_metric <- input$metric
 
       # print(params_metrics_choice_analysis()$index$metric_type_values[[input$metric]])
       # create metric description
@@ -139,12 +156,12 @@ mod_metric_analysis_server <- function(id, r_val){
                      radioButtons(ns("man_grouping_scale_select"),
                                   "Base de classification",
                                   c("Région", "Axe fluvial"),
-                                  selected = "Region",
+                                  selected = "Région",
                                   inline = TRUE)
                    )
             ),
             column(width = 7,
-                   uiOutput(ns("man_grouping_editable_tableUI")),
+                   rHandsontableOutput(ns("man_grouping_editable_table"), width = "100%"),
                    actionButton(inputId = ns("man_grouping_apply_changes"), "Appliquer")
             )
           )
@@ -152,9 +169,71 @@ mod_metric_analysis_server <- function(id, r_val){
       )
     })
 
-    # apply selection to global reactive values
-    observeEvent(input$metric, {
-      r_val$selected_metric <- input$metric
+    #### classification inputs changed ####
+    observeEvent(list(input$metric,
+                      input$man_grouping_quantile,
+                      input$man_grouping_no_classes,
+                      input$man_grouping_scale_select
+    ), {
+
+      # track input
+      track_inputs(input = input)
+
+      # check for valid values
+      if (!is.null(input$metric) &
+          !is.null(input$man_grouping_scale_select) &
+          !is.null(input$man_grouping_quantile) &
+          !is.null(input$man_grouping_no_classes)) {
+
+        if ((input$man_grouping_scale_select == "Région") &
+            !is.null(r_val$network_region)) {
+
+          # create classes-table
+          r_val_local$grouping_table_data = create_df_input(
+            axis_data = r_val$network_region,
+            variable_name = input$metric,
+            no_classes = input$man_grouping_no_classes,
+            quantile = input$man_grouping_quantile
+          )
+        }
+
+        else if (input$man_grouping_scale_select == "Axe fluvial" &
+                 !is.null(r_val$dgo_axis) ) {
+
+          # create classes-table
+          r_val_local$grouping_table_data = create_df_input(
+            axis_data = r_val$dgo_axis,
+            variable_name = input$metric,
+            no_classes = input$man_grouping_no_classes,
+            quantile = input$man_grouping_quantile
+          )
+        }
+      }
     })
+
+    # update table when values are edited (either via editing the table or setting the variables in the UI)
+    observeEvent(r_val_local$grouping_table_data, {
+
+      if (!is.null(r_val_local$grouping_table_data)) {
+        output$man_grouping_editable_table <- renderRHandsontable({
+          tmp <- isolate(r_val_local$grouping_table_data %>% select(!variable))# Gotta isolate it or it'll cause infinite loop, see https://github.com/jrowen/rhandsontable/issues/166
+          rownames(tmp) <- NULL
+          rhandsontable( tmp, rowHeaders = NULL) %>%
+            hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE)
+        })
+      }
+    })
+
+    # Update the reactive values when user edits table in the UI
+    observeEvent(input$man_grouping_editable_table, {
+
+      r_val_local$grouping_table_data <- hot_to_r(input$man_grouping_editable_table) %>%
+        bind_cols(
+          r_val_local$grouping_table_data %>%
+            select(variable)
+        )
+    })
+
+
   })
 }
