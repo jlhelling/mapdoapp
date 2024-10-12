aa91dbed5732b7a2a0b47c0056f6052140c012c5
jlhelling <128162318+jlhelling@users.noreply.github.com>
2024-10-12 13:50:41 +0200

    updated documentation page

--- R/mod_documentation.R
+++ R/mod_documentation.R
@@ -12,20 +12,35 @@ mod_documentation_ui <- function(id){
   tagList(
     golem_add_external_resources(),
     fluidPage(
-      HTML("
-            <p>MAPD&#39;O est un projet visant le d&eacute;veloppement d&#39;une interface web &agrave; l&#39;intention des op&eacute;rateurs autour des donn&eacute;es et mod&egrave;les en hydromorphologie. C&#39;est un projet r&eacute;alis&eacute; par le laboratoire Environnement Ville Soci&eacute;t&eacute; du CNRS et port&eacute; par l&#39;Office Fran&ccedil;ais de la Biodiversit&eacute; depuis janvier 2023.</p>
+      fluidRow(
+        column(
+          width = 9,
+          h1("Le projet"),
+          HTML("<p style='text-align: justify;'>
+          <b>Mapd'O</b> est un projet envisageant le développement d'une interface web d'analyse de données hydromorphologiques et de modèles pour les opérateurs. Le projet est porté par le laboratoire <i>CNRS UMR 5600 - Environnement Ville Société (EVS)</i> et est soutenu par <i>l'Office Français de la Biodiversité</i> depuis janvier 2023. <br> <br>
 
-            <p>L&#39;application repose sur des approches g&eacute;omatiques d&#39;analyse de la topographie et de l&#39;occupation du sol permettant de produire une carte de continuit&eacute; lat&eacute;rale des cours d&#39;eau du r&eacute;seau hydrographique. Diff&eacute;rentes m&eacute;triques morphologiques sont ensuite extraites &agrave; intervalle r&eacute;gulier du r&eacute;seau (largeur, pente, surface drain&eacute;e, &eacute;l&eacute;vation). L&#39;application a pour ambition de fournir des outils d&#39;analyse et d&#39;interpr&eacute;tation de ces m&eacute;triques afin de faciliter les diagnostics hydromorphologiques des cours d&#39;eau fran&ccedil;ais &agrave; l&#39;&eacute;chelle du bassin versant.</p>
+            L'application est basée sur les résultats de la <a href='https://tramebleue.github.io/fct-cli/' target='_blank' style='text-decoration: underline;'>Fluvial Corridor Toolbox</a>, également développée par le laboratoire EVS. Ces outils fournissent des approches géomatiques à l'analyse de la topographie et de l'occupation des sols, permettant la production de cartes de la continuité latérale et d'autres caractéristiques des cours d'eau. Les couches de fond de vallée et de corridor fluvial sont d'abord extraites d'un modèle numérique d'élévation. Diverses mesures morphologiques sont ensuite extraites du réseau à intervalles réguliers (occupation et utilisation du sol, largeur, pente, surface drainée, altitude).
 
-            <p>L&#39;application dispose aujourd&#39;hui d&#39;un module d&#39;exploration des donn&eacute;es permettant de visualiser les diff&eacute;rentes m&eacute;triques mises &agrave; disposition pour l&#39;analyse de bassin versant.</p>
-           "),
-      HTML("
-            <p><strong>Contact</strong> : Lise Vaudor (CNRS UMR 5600, Environnement Ville Soci&eacute;t&eacute;) - <a href='mailto:louis.maniere@ens-lyon.fr?subject=Application%20MAPDO'>lise.vaudor@ens-lyon.fr</a></p>
-         "),
+L'objectif de l'application est de fournir des outils d'analyse et d'interprétation de ces métriques afin de faciliter les diagnostics hydromorphologiques des cours d'eau français à différentes échelles (bassin versant, sous-bassin/région, axe fluvial). </p>
+               ")
+        ),
+        column(
+          width = 3,
+          h2("Contact"),
+          HTML("
+            Lise Vaudor <br>
+            <a href='mailto:lise.vaudor@ens-lyon.fr?subject=Application%20MAPDO'>lise.vaudor@ens-lyon.fr</a> <br>
+            CNRS UMR 5600, Environnement Ville Soci&eacute;t&eacute; <br>
+            <a href='https://umr5600.cnrs.fr/' target='_blank'>https://umr5600.cnrs.fr/</a></p>
+         ")
+        )
+      ),
+
+
       tags$a(
         href = "https://evs-gis.github.io/mapdowebsite/",
         icon("book"),
-        "Toute la documentation est ici",
+        "Cliquez ici pour ouvrir la documentation et en savoir plus sur le projet et sa méthodologie.",
         target = "_blank"
       )
     )
